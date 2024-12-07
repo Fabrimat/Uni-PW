d1916a67c28f8e3cf96dc4002abfb3c3129c8535
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2024-12-07 18:35:58 +0100

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE utenti (Nome TEXT, Cognome TEXT, Email TEXT, Telefono TEXT);  
+INSERT INTO utenti VALUES ('Cwawqs', 'Cblhfg', 'hpyzwm.xtuktr@demo.com', '+39 394 6657386');  
+INSERT INTO utenti VALUES ('Vhwlhn', 'Kfczew', 'vvpoia.okrqlp@test.com', '+39 382 5274810');  
+INSERT INTO utenti VALUES ('Zhfgfw', 'Mitycq', 'sftatf.tbajdx@demo.com', '+39 309 5649670');  
+INSERT INTO utenti VALUES ('Cocjmd', 'Ptgttj', 'wdckpb.ymktci@test.com', '+39 348 6722929');  
+INSERT INTO utenti VALUES ('Ewrblo', 'Fxuyrw', 'cvrmvn.psmxrr@test.com', '+39 313 1653915');  
+INSERT INTO utenti VALUES ('Tasnae', 'Prxadi', 'fywpqq.akprtc@test.com', '+39 393 4458323');  
+INSERT INTO utenti VALUES ('Tzgiad', 'Vingcx', 'agqagv.cqrelx@test.com', '+39 330 9046181');  
+INSERT INTO utenti VALUES ('Yogsbr', 'Zllooy', 'rvvtnn.hrsduj@test.com', '+39 321 3073595');  
+INSERT INTO utenti VALUES ('Tpsrgm', 'Ruptan', 'ojpigb.pgkxzo@esempio.com', '+39 357 9259264');  
+INSERT INTO utenti VALUES ('Pmgkii', 'Ewqcyz', 'utgdxv.odxlcf@esempio.com', '+39 387 3466102'); 
